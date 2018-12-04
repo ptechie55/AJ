e513a2f7dda55e1f3b4f5d01d4cdb8668eaebbf0
ptechie55 <prasanthc.techie@gmail.com>
2018-12-03 22:56:16 -0600

    second connit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
+  declarations: [ 
     AppComponent
   ],
   imports: [
